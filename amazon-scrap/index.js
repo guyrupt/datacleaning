a5d2c03903e2b7b53ca86527d673ef7e9bf195dd
Johnson <hungjiewu@gmail.com>
2023-11-18 19:41:41 -0500

    feat: add proxy

--- amazon-scrap/index.js
+++ amazon-scrap/index.js
@@ -5,7 +5,7 @@ const cliProgress = require('cli-progress');
 const { uploadCSV } = require('./s3.js');
 
 const keywords = ["Laptop", "Smartphone", "Headphones", "Bluetooth Speaker", "Kitchenware", "Sports Equipment", "Fitness Tracker", "Men's Shoes", "Women's Shoes", "Children's Clothing", "Novels", "Textbooks", "Groceries", "Pet Supplies", "Cosmetics", "Skin Care", "Hair Care", "Toys", "Video Games", "Gardening Tools", "Outdoor Furniture", "Indoor Furniture", "Cookbooks", "Musical Instruments", "Car Accessories", "Bike", "Watches", "Jewelry", "Sunglasses", "Handbags", "Backpacks", "Luggage", "Cameras", "Printers", "Office Supplies", "Stationery", "Board Games", "Craft Supplies", "Home Decor", "Bedding", "Kitchen Appliances", "Television", "Audio Equipment", "Smart Home Devices", "Books", "Ebooks", "Magazines", "Music CDs", "Vinyl Records", "DVDs", "Blu-Ray Discs", "Baby Clothing", "Baby Gear", "Maternity Clothing", "Men's Clothing", "Women's Clothing", "Men's Accessories", "Women's Accessories", "Shoes", "Socks", "Underwear", "Swimwear", "Electronics", "Computer Accessories", "Software", "Hardware Tools", "Painting Supplies", "Plumbing Equipment", "Lighting Fixtures", "Camping Gear", "Fishing Equipment", "Hiking Gear", "Bathroom Accessories", "Kitchen Gadgets", "Coffee Maker", "Tea Accessories", "Wine Accessories", "Grilling Tools", "Yoga Mat", "Workout Clothing", "Running Shoes", "Fitness DVDs", "Protein Powder", "Vitamins", "Supplements", "First Aid Supplies", "Prescription Glasses", "Contact Lenses", "Sewing Machine", "Knitting Supplies", "Scrapbooking Materials", "Party Decorations", "Gift Wrapping Supplies", "Holiday Decorations", "Costumes", "Candles", "Essential Oils", "Cleaning Supplies", "Laundry Detergent", "Vacuum Cleaner"]
-
+const proxyList = ["143.110.190.83:8080", "117.2.218.16:4006", "64.225.8.132:10000", "191.243.46.162:43241"]
 
 const getProductByKeyword = async () => {
     const searchProducts = []
@@ -21,7 +21,7 @@ const getProductByKeyword = async () => {
                 keyword: keyword, 
                 number:50, 
                 rating: [4, 5],
-                proxy:['143.110.190.83:8080']
+                proxy: proxyList
             });
         } catch (err) {
             console.log(err);
@@ -57,7 +57,11 @@ const getProductByKeyword = async () => {
 }
 
 const getReviewsByASIN = async (asin) => {
-    const reviews = await amazonScraper.reviews({ asin: asin, rating: [4, 5] });
+    const reviews = await amazonScraper.reviews({ 
+        asin: asin, 
+        rating: [4, 5],
+        proxy: proxyList
+    });
 
     const firstReview = reviews.result[0];
 
